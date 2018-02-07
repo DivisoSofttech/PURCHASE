1c9aad9df4201448b812c6df2f6c2f8dc7db9ff1
vishnuviki <vishnu.p.v@lxisoft.com>
2018-02-07 10:46:11 +0530

    remove all files

--- src/main/java/com/diviso/purchase/config/audit/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Audit specific code.
- */
-package com.diviso.purchase.config.audit;
--- src/main/java/com/diviso/purchase/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * View Models used by Spring MVC REST controllers.
- */
-package com.diviso.purchase.web.rest.vm;
